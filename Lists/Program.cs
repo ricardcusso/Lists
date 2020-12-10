e5d5571f7b6dc03e402cbdb57f22250c9580524c
Ricard <ricard.cusso@gmail.com>
2020-12-10 23:00:53 +1000

    Exercise 1 Finished

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -14,41 +14,38 @@ namespace Lists
 
             while(!isUserFinished)
             {
-                if(Console.ReadLine() == "")
+                var userInput = Console.ReadLine();
+
+                if(userInput.Length == 0)
                 {
                     isUserFinished = true;
-                    break;
                 }
                 else
                 {
-                    namesList.Add(Console.ReadLine());
+                    namesList.Add(userInput);
                 }
             }
 
             if(namesList.Count == 0)
             {
-                Console.WriteLine(namesList.Count);
                 Console.WriteLine("You have no friends, loser.");
                 return;
             }
 
             else if(namesList.Count == 1)
             {
-                Console.WriteLine(namesList.Count);
                 Console.WriteLine($"{namesList[0]} is your friend.");
                 return;
             }
 
             else if(namesList.Count == 2)
             {
-                Console.WriteLine(namesList.Count);
                 Console.WriteLine($"{namesList[0]} and {namesList[1]} are your friends.");
                 return;
             }
 
             else
             {
-                Console.WriteLine(namesList.Count);
                 Console.WriteLine($"{namesList[0]}, {namesList[1]} and {(namesList.Count) - 2} others are your friends.");
                 return;
             }
